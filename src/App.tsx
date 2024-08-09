568d17d4aa0547278139e4d8a375cd3228b94147
w-rahul <rahulguptaji1019@gmail.com>
2024-08-09 14:04:24 +0530

    Initial file set-up

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div className="p-4 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+      <h2 className="text-2xl font-bold text-center text-blue-500">Tailwind CSS with Vite</h2>
+      <p className="text-gray-700">This is a sample component to check Tailwind CSS integration with React and TypeScript using Vite.</p>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded">Click Me</button>
+    </div>
+    </>
+  )
+}
+
+export default App
