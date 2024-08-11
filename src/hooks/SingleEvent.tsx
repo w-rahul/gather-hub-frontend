aa07b20cb5714f8a1022dcf94e30d67a1c9cfaa9
w-rahul <rahulguptaji1019@gmail.com>
2024-08-11 14:49:26 +0530

    Created SingleEvent hook

---
+++ src/hooks/SingleEvent.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+
+interface SingleEvent {
+    id : string
+    title : string
+    description : string
+    location : string
+    category : string
+    date : string
+    
+}
+
+const useSingleEvent = () =>{
+    useEffect(()=>{
+
+    },[])
+}
