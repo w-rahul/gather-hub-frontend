7e43bf895937b5b4e3a93f9c21c1c727f4e5e8aa
w-rahul <rahulguptaji1019@gmail.com>
2024-08-12 13:45:48 +0530

    Added UserIdFromToken

---
+++ src/hooks/UserIdFromToken.tsx
@@ -0,0 +1,13 @@
+import { jwtDecode } from "jwt-decode"
+
+export const UserIdFromToken = () =>{ 
+    const Token:string | null = localStorage.getItem("token")
+    let UserID :string = ''
+
+    if(Token){
+        const DecodedToken = jwtDecode(Token) as {id :string}
+        UserID = DecodedToken.id || " " 
+    }
+    
+    return UserID
+}
