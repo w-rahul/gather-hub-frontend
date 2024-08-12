bb135d0c27856c901f65e14e728d2efcad277595
w-rahul <rahulguptaji1019@gmail.com>
2024-08-12 18:20:46 +0530

    Added register button

--- src/components/CreateEvent.tsx
+++ src/components/CreateEvent.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react"
 import { InputBox2 } from "./Input"
+import { ButtonComp } from "./Button"
+import axios from "axios"
+import { BACKEND_URL } from "../config"
+import { UserIdFromToken } from "../hooks/UserIdFromToken"
+import { useNavigate } from "react-router-dom"
 
 
 export const CreateEvent = () => {
@@ -7,13 +12,16 @@ export const CreateEvent = () => {
     const [title, settitle] = useState("") 
     const [description, setdescription] = useState("") 
     const [location, setlocation] = useState("") 
-    const [name, setname] = useState("") 
     const [Category, setCategory] = useState("") 
     const [date, setdate] = useState("")
+    const Navigate = useNavigate()
 
-    return   <div className="h-screen flex items-center justify-center items w-full">
-    <div className="rounded-2xl w-1/3 max-h-screen min-h-96 grid p-2 grid-rows-[60%_40%] bg-zinc-900">
-        <div className="p-3">  
+    const UserIdByToken = UserIdFromToken()
+
+    return <div> 
+     <div className="h-screen flex items-center justify-center items w-full">
+        <div className="rounded-2xl w-1/3 max-h-screen min-h-96 grid p-2 grid-rows-[55%_50%] bg-zinc-900">
+         <div className="p-3">  
         <div className=" p-4 text-4xl font-bold font-serif">
         <InputBox2 onchange={(e)=>{
             settitle(e.target.value)
@@ -43,18 +51,42 @@ export const CreateEvent = () => {
                     <div className="h-full">
                     <InputBox2 onchange={(e)=>{
           setdate(e.target.value)
-        }} label="Date" placeholder="Add Date" />
+        }} label="Date" type="date" placeholder="Add Date" />
                     </div>
 
-                    <div className="h-full">
-                    <InputBox2 onchange={(e)=>{
+                    <div className=" h-full flex justify-center mt-9">
+                    {/* <InputBox2 onchange={(e)=>{
             setname(e.target.value)
-        }} label="Name" placeholder="Add Organizer Name" />
+        }} label="Name" placeholder="Add Organizer Name" /> */}
+        <div>
+             <ButtonComp onclick={async ()=>{
+                try {
+                    await axios.post(`${BACKEND_URL}`,{
+                        title: title,
+                        description: description,
+                        date: date,
+                        location: location,
+                        category: Category,
+                        organizerId : UserIdByToken
+                    },{
+                        headers:{
+                            Authorization : "Bearer " + localStorage.getItem("token")
+                        }
+                    })
+                    Navigate("/")
+                } catch (error) {
+                      console.error("Error occured while creating event " + error)                  
+                }
+
+             }} label="Register" width="w-36"/>
+    </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+    
 </div>
 }
 
