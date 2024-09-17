bfd2911eb2e7d2cbd9b8780e196417e3e96fd175
Rahul <rahulguptaji1019@gmail.com>
2024-09-17 17:52:29 +0530

    Added responsiveness

--- src/pages/SingleEvent.tsx
+++ src/pages/SingleEvent.tsx
@@ -4,8 +4,6 @@ import { useSingleEvent } from "../hooks/SingleEvent"
 import { Appbar } from "../components/Appbar"
 import { useCustomTitle } from "../hooks/CustomTitle"
 export const FullEvent = () =>{
-
-    
     useCustomTitle('GatherHub | Event')
 
     const Navigate = useNavigate()
@@ -13,21 +11,20 @@ export const FullEvent = () =>{
     const {loading, SingleEventdata} = useSingleEvent({id : id || ""})
 
     if(loading || !SingleEventdata){
-        return <div className="w-full h-screen flex items-center bg-black justify-center text-white font-bold text-3xl">
-        Loading.....
-    </div>
+        return <div className="w-full h-screen flex items-center bg-black justify-center text-white font-bold text-xl md:text-3xl">
+            Loading.....
+        </div>
     }
 
-    return <div className="bg-black w-full h-screen overflow-hidden text-white">
-        <div><Appbar width="w-20" label="Logout" onclick={()=>{
-                    localStorage.removeItem("token")
-                    Navigate("/login")
-        }}/>
+    return <div className="bg-black w-full min-h-screen overflow-x-hidden text-white">
+        <div className="sticky top-0 z-10">
+            <Appbar width="w-full md:w-20" label="Logout" onclick={()=>{
+                localStorage.removeItem("token")
+                Navigate("/login")
+            }}/>
         </div>
-        <div>
+        <div className="p-4 md:p-8">
             <SingleEvent hello={SingleEventdata} />
-
         </div>
-        
     </div>
 }
