4ee82e438c44cac917756b049f261af520aa45f5
Rahul <rahulguptaji1019@gmail.com>
2024-09-17 17:49:41 +0530

    Added responsiveness

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 export const ButtonComp =({label, onclick, width}: {label?:string, onclick?:()=> void, width?:string }) =>{
-    return <div>
-        <button onClick={onclick} className={`bg-white text-black text-center ${ width || "w-full"} py-3 font-semibold rounded-xl hover:bg-zinc-300`}>{label}</button>
+    return <div className="w-full">
+        <button onClick={onclick} className={`bg-white text-black text-center ${width || "w-full"} py-2 md:py-3 text-sm md:text-base font-semibold rounded-xl hover:bg-zinc-300 transition duration-300`}>{label}</button>
     </div>
 }
